ee676485590aaad34fba5f1f508e38ad89ff38dc
prinamehta <135137091+prinamehta@users.noreply.github.com>
2025-03-02 14:27:11 -0800

    Added cinemachine camera and walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,53 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed;
+    public float moveSpeed = 5f;
     public Rigidbody playerRB;
-    public float jumpForce;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float jumpForce = 7f;
+    private bool isGrounded;
+    
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        playerRB.linearVelocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, playerRB.linearVelocity.y, Input.GetAxis("Vertical") * moveSpeed);
-
-        if(Input.GetButtonDown("Jump"))
+        Move();
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            playerRB.linearVelocity = new Vector3(playerRB.linearVelocity.x, jumpForce, playerRB.linearVelocity.z);
+            Jump();
         }
-        
+    }
+
+    void Move()
+    {
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
+
+    Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized; // Normalized to avoid faster diagonal movement
+
+    if (direction.magnitude > 0.1f) // Prevents jittery rotation when idle
+    {
+        transform.forward = direction; // Rotate player in movement direction
+    }
+
+    playerRB.MovePosition(transform.position + direction * moveSpeed * Time.deltaTime);
+    }
+
+
+    void Jump()
+    {
+        playerRB.linearVelocity = new Vector3(playerRB.linearVelocity.x, jumpForce, playerRB.linearVelocity.z);
+        isGrounded = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
